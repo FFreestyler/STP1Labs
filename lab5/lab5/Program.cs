4294164e62c5039c2028a47131d110e613d4e3df
FFreestyler <freeestyler231@gmail.com>
2022-09-13 16:56:06 +0700

    Added lab5 without tests

---
+++ lab5/lab5/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using lab5;
+
+try
+{
+    SimpleFractionClass f = new SimpleFractionClass(1, 5);
+    SimpleFractionClass f2 = new SimpleFractionClass(2, 5);
+    SimpleFractionClass outn;
+
+    outn = f + f2;
+
+    outn.Show();
+
+    Console.WriteLine(outn.ToString());
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}

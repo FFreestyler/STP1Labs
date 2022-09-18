d7ae963e97b4a8ab9817cdfc0a61f8053fcadae2
RuhidKa <ferein01@gmail.com>
2022-09-18 22:49:53 +0700

    deleted stp labs

--- lab1/lab1/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using lab1;
-
-var arr = new float[] { 1, 2, 3, 4 };
-foreach (var item in Class1.ShiftArray(arr, 2))
-{
-    Console.WriteLine(item);
-}
